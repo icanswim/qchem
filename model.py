9c5478da82da664e8cd8177b4790463b0f502fe5
wylderhayes <wylderhayes@gmail.com>
2023-09-12 13:27:48 -0700

    upgrade to GraphNet

--- model.py
+++ model.py
@@ -19,36 +19,40 @@ def pyg_model(model_params):
     print('pytorch geometric model {} loaded...'.format(model_params['model_name']))
     return model
 
+
+
 class GraphNet(CModel):
     
-    def build(self, in_channels=0, hidden=0, out_channels=0, depth=0):
+    def build(self, in_channels=0, hidden=0, out_channels=0, depth=0, 
+                      Conv=GCNConv, pool=global_mean_pool):
         self.layers = []
-        self.layers.append(GCNConv(in_channels, hidden))
+        self.layers.append(Conv(in_channels, hidden))
         for d in range(depth):
-            self.layers.append(GCNConv(hidden, hidden))
-        self.ffu = CModel.ff_unit(self, hidden, out_channels, activation=None, 
-                                                      batch_norm=False, dropout=.2)
+            self.layers.append(Conv(hidden, hidden))
+        self.pool = pool
+        self.ffn = FFNet({'in_channels':hidden, 'hidden':2*hidden, 'out_channels':out_channels})
                                
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         for l in self.layers:
             x = l(x, edge_index)
             x = F.relu(x)
-        x = global_mean_pool(x, data.batch)
-        x = self.ffu(x)
+        if self.pool is not None:
+            x = self.pool(x, data.batch)
+        x = self.ffn(x)
         return x
     
 class GlobalAttentionNet(CModel):
     
     def build(self, in_channels=0, hidden=0, out_channels=0, depth=0):
         self.layers = []
-        self.layers.append(SAGEConv(in_channels, hidden))
+        self.layers.append(SAGEConv(in_channels, hidden, normalize=True))
         for d in range(depth):
-            self.layers.append(SAGEConv(hidden, hidden))
+            self.layers.append(SAGEConv(hidden, hidden, normalize=True))
         self.attention = AttentionalAggregation(CModel.ff_unit(self, hidden, 1, 
                                    activation=None, dropout=None, batch_norm=False))
         self.ffu1 = CModel.ff_unit(self, hidden, hidden, 
-                                   activation=nn.ReLU, dropout=.2, batch_norm=False)
+                                   activation=nn.ReLU, dropout=.2, batch_norm=True)
         self.ffu2 = CModel.ff_unit(self, hidden, out_channels, 
                                    activation=None, dropout=None, batch_norm=False)
                           
