e4e220b2df480bba941923d50a8159b9e893fff2
Scott Ward <wardsj@hotmail.com>
2025-01-03 13:44:48 -0800

    merge from comosis.learning updates

--- learning.py
+++ learning.py
@@ -11,17 +11,13 @@ import matplotlib.pyplot as plt
 
 from torch import no_grad, save, load, from_numpy, squeeze
 from torch.utils.data import Sampler, DataLoader
-from torch.nn.functional import softmax
 
-from sklearn import metrics
-
-from model import EncoderLoss
+from sklearn import metrics as sk_metrics
 
 
 class Metrics():
-
-    def __init__(self, report_interval=10, sk_metric_name=None, 
-                     log_plot=False, min_lr=.00125, sk_param={}):
+    def __init__(self, report_interval=10, metric_name=None, 
+                     log_plot=False, min_lr=.00125, metric_param={}):
         
         self.start = datetime.now()
         self.report_time = self.start
@@ -29,62 +25,100 @@ class Metrics():
         self.log_plot = log_plot
         self.min_lr = min_lr
         
-        self.epoch, self.e_loss, self.predictions = 0, [], []
-        self.train_loss, self.val_loss, self.lr_log = [], [], []
+        self.epoch, self.e_loss, self.predictions, self.lr_log = 0, [], [], []
+        self.train_loss, self.val_loss, self.test_loss = [], [], []
         
-        self.sk_metric_name, self.sk_param = sk_metric_name, sk_param
-        self.skm, self.sk_train_log, self.sk_val_log = None, [], []
-        self.y, self.last_y, self.y_pred, self.last_y_pred = [], [], [], []
-        if self.sk_metric_name is not None:
-            self.skm = getattr(metrics, self.sk_metric_name)
+        self.metric_name, self.metric_param = metric_name, metric_param
+        self.metric_func, self.metric_train_log, self.metric_val_log = None, [], []
+        self.y, self.y_pred, = [], []
+        
+        if self.metric_name is not None:
+            if self.metric_name == 'transformer':
+                self.metric_func = None
+            else:
+                self.metric_func = getattr(sk_metrics, self.metric_name)
             
         logging.basicConfig(filename='./logs/cosmosis.log', level=20)
         self.log('\nNew Experiment: {}'.format(self.start))
     
     def infer(self):
-        self.predictions = np.concatenate(self.predictions)
-        self.predictions = pd.DataFrame(self.predictions)
-        self.predictions.to_csv('./logs/{}_inference.csv'.format(self.start), index=True)
+        
+        if self.metric_name != 'transformer':
+            self.predictions = np.concatenate(self.predictions).squeeze()
+        
+        pd.DataFrame(self.predictions).to_csv(
+                        './logs/{}_inference.csv'.format(self.start), index=True)
         print('inference {} complete and saved to csv...'.format(self.start))
 
     def metric(self, flag):
-        """TODO multiple sk metrics"""
-    
+        """TODO multiple metric"""
+        self.flag = flag 
+        self.display_y, self.display_y_pred = [], []
+        
         def softmax(x): return np.exp(x)/sum(np.exp(x))
 
         def softmax_overflow(x):
             x_max = x.max()
             normalized = np.exp(x - x_max)
             return normalized / normalized.sum()
-
-        y = np.concatenate(self.y)
-        y_pred = np.concatenate(self.y_pred)
-        
-        if self.sk_metric_name is not None:
-            if self.sk_metric_name == 'roc_auc_score' and y_pred.ndim == 2:
-                y_pred = np.apply_along_axis(softmax_overflow, 1, y_pred)
             
-            if self.sk_metric_name == 'accuracy_score' and y_pred.ndim == 2:
-                y_pred = np.argmax(y_pred, axis=1)
+        y_pred = self.y_pred
+        
+        if self.metric_name == 'transformer':
+            y_pred = y_pred[-1]
+            if y_pred.ndim == 3: y_pred = y_pred[-1]
+            y_pred = np.apply_along_axis(softmax_overflow, 0, np.squeeze(y_pred))
+            y_pred = np.apply_along_axis(np.argmax, 0, y_pred).squeeze().flatten().tolist()
+            y_pred = self.decoder(y_pred)
+            self.display_y_pred = y_pred
+        else:
+            y_pred = np.concatenate(y_pred)
+            
+        if self.metric_name in ['roc_auc_score']:
+            y_pred = np.apply_along_axis(softmax_overflow, 1, y_pred)
+            self.display_y_pred = y_pred
+            
+        elif self.metric_name in ['accuracy_score']:
+            y_pred = np.argmax(y_pred, axis=1)
+            self.display_y_pred = y_pred
+        else:
+            self.display_y_pred = y_pred
 
-            score = self.skm(y, y_pred, **self.sk_param)
+        if flag == 'infer':
+            self.predictions.append(y_pred)
+            return
+
+        y = self.y
+        
+        if self.metric_name == 'transformer':
+            y = y[-1]
+            if y.ndim == 2: y = y[-1]
+            y = y.squeeze().flatten().tolist() 
+            self.display_y = self.decoder(y)
+        else:
+            y = np.concatenate(self.y)
+            self.display_y = y[-5:]
+        
+        if self.metric_func != None:
+            score = self.metric_func(y, y_pred, **self.metric_param)
             if flag == 'train':
-                self.sk_train_log.append(score)
+                self.metric_train_log.append(score)
             else:
-                self.sk_val_log.append(score)
+                self.metric_val_log.append(score)
 
-        self.last_y, self.last_y_pred = np.squeeze(y[-5:]), np.squeeze(y_pred[-5:])
+    def flush(self):
         self.y, self.y_pred = [], []
         
-    def loss(self, flag, loss):
+    def loss(self, flag, loss=None):
         if flag == 'train':
             self.train_loss.append(loss)
         if flag == 'val':
             self.val_loss.append(loss)
         if flag == 'test':
-            self.log('test loss: {}'.format(loss))
-            print('test loss: {}'.format(loss))
-          
+            self.test_loss.append(loss)
+        if flag == 'infer':
+            return
+        
     def log(self, message):
         logging.info(message)
         
@@ -94,12 +128,12 @@ class Metrics():
             print('\n...........................')
             print('learning time: {}'.format(datetime.now()-self.start))
             print('epoch: {}, lr: {}'.format(self.epoch, self.lr_log[-1]))
-            print('train loss: {}, val loss: {}'.format(self.train_loss[-1], self.val_loss[-1]))
-            print('last 5 targets: \n{}'.format(self.last_y))
-            print('last 5 predictions: \n{}'.format(self.last_y_pred))
-            if self.skm is not None:
-                print('sklearn train metric: {}, sklearn validation metric: {}'.format(
-                                                    self.sk_train_log[-1], self.sk_val_log[-1]))
+            print('train loss: {}, val loss: {}\n'.format(self.train_loss[-1], self.val_loss[-1]))
+            print('last targets: \n{}\n'.format(self.display_y))
+            print('last predictions: \n{}\n'.format(self.display_y_pred))
+            if len(self.metric_train_log) != 0:
+                print('{} train score: {}, validation score: {}'.format(
+                    self.metric_name, self.metric_train_log[-1], self.metric_val_log[-1]))
             self.report_time = datetime.now()
         
         if now:
@@ -114,20 +148,26 @@ class Metrics():
         print('\n...........................')
         self.log('learning time: {} \n'.format(elapsed))
         print('learning time: {}'.format(elapsed))
-        print('last 5 targets: \n{}'.format(self.last_y))
-        print('last 5 predictions: \n{}'.format(self.last_y_pred))
+        print('last predictions: \n{}\n'.format(self.display_y_pred))
+
+        if self.flag != 'infer': 
+            print('last targets: \n{}'.format(self.display_y))
+            print('test loss: {} \n'.format(self.test_loss[-1]))
+            self.log('test loss: {} \n'.format(self.test_loss))
         
-        if self.skm is not None:
-            self.log('sklearn test metric: \n{} \n'.format(self.sk_val_log[-1]))
-            print('sklearn test metric: \n{} \n'.format(self.sk_val_log[-1]))
-            logs = zip(self.train_loss, self.val_loss, self.lr_log, self.sk_val_log)
-            cols = ['train_loss','validation_loss','learning_rate',self.sk_metric_name]
-        else:
-            logs = zip(self.train_loss, self.val_loss, self.lr_log)
-            cols = ['train_loss','validation_loss','learning_rate']
+            if len(self.metric_train_log) != 0:
+                self.log('{} test metric: \n{} \n'.format(self.metric_name, self.metric_val_log[-1]))
+                print('{} test metric: \n{} \n'.format(self.metric_name, self.metric_val_log[-1]))
+                logs = zip(self.train_loss, self.val_loss, self.lr_log, self.metric_val_log)
+                cols = ['train_loss','validation_loss','learning_rate',self.metric_name]
+            else:
+                logs = zip(self.train_loss, self.val_loss, self.lr_log)
+                cols = ['train_loss','validation_loss','learning_rate']
             
-        pd.DataFrame(logs, columns=cols).to_csv('./logs/'+self.start.strftime("%Y%m%d_%H%M"))
-        self.view_log('./logs/'+self.start.strftime('%Y%m%d_%H%M'), self.log_plot)
+            pd.DataFrame(logs, columns=cols).to_csv('./logs/'+self.start.strftime("%Y%m%d_%H%M"))
+            self.view_log('./logs/'+self.start.strftime('%Y%m%d_%H%M'), self.log_plot)
+        else:
+            self.infer()
         
     @classmethod    
     def view_log(cls, log_file, log_plot):
@@ -197,8 +237,6 @@ class Selector(Sampler):
         return self
     
     def shuffle_train_val_idx(self):
-        if self.set_seed:
-            random.seed(self.set_seed)
         random.shuffle(self.val_idx)
         random.shuffle(self.train_idx)
         random.seed()
@@ -214,11 +252,17 @@ class Learn():
     Criterion = None implies inference mode
     
     load_model = None/'saved_model.pth'/'saved_model.pk'
+    
     load_embed = None/'model_name'
+    
     squeeze_y_pred = True/False (torch.squeeze(y_pred)) 
         squeeze the model output
+        
     adapt = (D_in, D_out, dropout)
         prepends a trainable linear layer
+
+    weights_only = True/False
+        enable un pickling of models = False (only unpickle trusted files)
         
     the dataset output can either be a dictionary utilizing the form 
     data = {'model_input': {},
@@ -232,8 +276,10 @@ class Learn():
                  ds_param={}, model_param={}, sample_param={},
                  opt_param={}, sched_param={}, crit_param={}, metrics_param={}, 
                  adapt=None, load_model=None, load_embed=None, save_model=False,
-                 batch_size=10, epochs=1, squeeze_y_pred=False, gpu=True, target='y'):
-        
+                 batch_size=10, epochs=1,
+                 gpu=True, weights_only=False, squeeze_y_pred=False, target='y'):
+
+        self.weights_only = weights_only
         self.gpu = gpu
         self.bs = batch_size
         self.squeeze_y_pred = squeeze_y_pred
@@ -243,6 +289,9 @@ class Learn():
         self.DataLoader = DataLoader
         
         self.metrics = Metrics(**metrics_param)
+        if hasattr(self.train_ds, 'encoding'):
+            self.metrics.decoder = self.train_ds.encoding.decode
+        
         self.metrics.log('model: {}\n{}'.format(Model, model_param))
         self.metrics.log('dataset: {}\n{}'.format(Datasets, ds_param))
         self.metrics.log('sampler: {}\n{}'.format(Sampler, sample_param))
@@ -254,10 +303,10 @@ class Learn():
         if load_model is not None:
             try: 
                 model = Model(model_param)
-                model.load_state_dict(load('./models/'+load_model))
+                model.load_state_dict(load('./models/'+load_model, weights_only=self.weights_only))
                 print('model loaded from state_dict...')
             except:
-                model = load('./models/'+load_model)
+                model = load('./models/'+load_model, weights_only=self.weights_only)
                 print('model loaded from pickle...')                                                      
         else:
             model = Model(model_param)
@@ -267,10 +316,10 @@ class Learn():
                 weight = np.load('./models/{}_{}_embedding_weight.npy'.format(load_embed, i))
                 embedding.from_pretrained(from_numpy(weight), freeze=model_param['embed_param'][i][4])
             print('loading embedding weights...')
-          
+                    
         if adapt is not None: model.adapt(*adapt)
         
-        if self.gpu:
+        if self.gpu == True:
             try:
                 self.model = model.to('cuda:0')
                 print('running model on gpu...')
@@ -286,20 +335,14 @@ class Learn():
         
         if Criterion is not None:
             self.criterion = Criterion(**crit_param)
-            if self.gpu: 
-                if isinstance(self.criterion, EncoderLoss):
-                    self.criterion.decoder.to('cuda:0')
-                    if self.criterion.adversarial:
-                        self.criterion.discriminator.to('cuda:0')
-                else: 
-                    self.criterion.to('cuda:0')
+            if self.gpu: self.criterion.to('cuda:0')
             self.metrics.log('criterion: {}\n{}'.format(self.criterion, crit_param))
             self.opt = Optimizer(self.model.parameters(), **opt_param)
             self.metrics.log('optimizer: {}\n{}'.format(self.opt, opt_param))
             self.scheduler = Scheduler(self.opt, **sched_param)
             self.metrics.log('scheduler: {}\n{}'.format(self.scheduler, sched_param))
             
-            for e in range(epochs): 
+            for e in range(epochs):
                 self.metrics.epoch = e
                 self.sampler.shuffle_train_val_idx()
                 self.run('train')
@@ -307,17 +350,18 @@ class Learn():
                     self.run('val')
                     if e > 1 and  self.metrics.lr_log[-1] <= self.metrics.min_lr:
                         self.metrics.status_report(now=True)
-                        print('early stopping!  learning rate is below the set minimum...')
+                        print('\n early stopping!  learning rate is below the set minimum...')
                         break
                 
             with no_grad():
                 self.run('test')
                 
-            self.metrics.final_report()  
-            
         else: #no Criterion implies inference mode
             with no_grad():
-                self.run('infer')
+                for e in range(epochs):
+                    self.run('infer')
+                    
+        self.metrics.final_report() 
         
         if save_model:
             if type(save_model) == str:
@@ -334,8 +378,8 @@ class Learn():
                     weight = embedding.weight.detach().cpu().numpy()
                     np.save('./models/{}_{}_embedding_weight.npy'.format(model_name, i), weight)
         
-    def run(self, flag): 
-        e_loss, e_sk, i = 0, 0, 0
+    def run(self, flag):
+        e_loss, i = 0, 0
         if flag == 'train': 
             self.model.training = True
             dataset = self.train_ds
@@ -355,7 +399,7 @@ class Learn():
             self.model.training = False
             dataset = self.test_ds
             drop_last = False
-
+        
         dataloader = self.DataLoader(dataset, batch_size=self.bs, 
                                      sampler=self.sampler(flag=flag), 
                                      num_workers=0, pin_memory=True, 
@@ -372,65 +416,60 @@ class Learn():
                 else: 
                     data = data.to('cuda:0', non_blocking=True)
             y_pred = self.model(data)
-     
             if self.squeeze_y_pred: y_pred = squeeze(y_pred)
                 
-            if flag == 'infer':
-                self.metrics.predictions.append(y_pred.detach().cpu().numpy())
-            else:
+            if flag != 'infer':
                 if type(data) == dict:
                     y = data[self.target]
                 else: 
                     y = getattr(data, self.target)
+                    
                 self.opt.zero_grad()
-                #variation from Cosmo
-                if isinstance(self.criterion, EncoderLoss):
-                    b_loss, y_pred, y = self.criterion(*y_pred, data, flag)
-                else:
-                    b_loss = self.criterion(y_pred, y)
+                #TODO variable in/out for criterion
+                b_loss = self.criterion(y_pred, y)
                 e_loss += b_loss.item()
-                
-                self.metrics.y.append(y.detach().cpu().numpy())
-                self.metrics.y_pred.append(y_pred.detach().cpu().numpy())
                 
                 if flag == 'train':
                     b_loss.backward()
                     self.opt.step()
                     
-        if flag == 'infer':
-            self.metrics.infer()
-        else:
-            self.metrics.loss(flag, e_loss/i)
-            self.metrics.metric(flag)
+                self.metrics.y.append(y.detach().cpu().numpy())
+                
+            self.metrics.y_pred.append(y_pred.detach().cpu().numpy())
             
+        if e_loss != 0: self.metrics.loss(flag, e_loss/i)
+        self.metrics.metric(flag)
+        
         if flag == 'val': 
-            self.scheduler.step(e_loss/i)
+            self.scheduler.step(e_loss)
             self.metrics.lr_log.append(self.opt.param_groups[0]['lr'])
             self.metrics.status_report()
             
+        self.metrics.flush()
+        
     def dataset_manager(self, Datasets, Sampler, ds_param, sample_param):
-    
+        
         if len(Datasets) == 1:
             self.train_ds = Datasets[0](**ds_param['train_param'])
             self.val_ds = self.test_ds = self.train_ds
             self.sampler = Sampler(dataset_idx=self.train_ds.ds_idx, 
-                                   **sample_param)
+                                       **sample_param)
+
         if len(Datasets) == 2:
             self.train_ds = Datasets[0](**ds_param['train_param'])
             self.val_ds = self.train_ds
             self.test_ds = Datasets[1](**ds_param['test_param'])
             self.sampler = Sampler(train_idx=self.train_ds.ds_idx, 
-                                   test_idx=self.test_ds.ds_idx,
-                                   **sample_param)
+                                       test_idx=self.test_ds.ds_idx,
+                                           **sample_param)
         if len(Datasets) == 3:
             self.train_ds = Datasets[0](**ds_param['train_param'])
             self.val_ds = Datasets[1](**ds_param['val_param'])
             self.test_ds = Datasets[2](**ds_param['test_param'])
             self.sampler = Sampler(train_idx=self.train_ds.ds_idx, 
-                                   val_idx=self.val_ds.ds_idx, 
-                                   test_idx=self.test_ds.ds_idx,
-                                   **sample_param)
-
+                                       val_idx=self.val_ds.ds_idx, 
+                                           test_idx=self.test_ds.ds_idx,
+                                               **sample_param)
 
 
         
